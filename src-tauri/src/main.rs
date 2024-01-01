4687c9eb7fc01624e3023e98ccc3b03b5e1b9506
wisdomstar94 <wisdomstar94@gmail.com>
2024-01-01 17:22:07 +0900

    v0.0.3 : 에러 발생하는 부분 주석 처리.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,7 +13,7 @@ use dotenv::dotenv;
 
 #[tauri::command]
 fn greet(name: &str, age: u32) -> String {
-  let api_token = std::env::var("API_TOKEN").expect("API_TOKEN must be set.");
-  println!("api_token is {}", api_token);
+  // let api_token = std::env::var("API_TOKEN").expect("API_TOKEN must be set.");
+  // println!("api_token is {}", api_token);
   format!("Hello, {}! your age is {}", name, age)
 }
